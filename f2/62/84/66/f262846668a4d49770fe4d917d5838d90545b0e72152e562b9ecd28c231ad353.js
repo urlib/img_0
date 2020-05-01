8e4535296a0ba4aeb1fdd1f0d2939c6dfdc3693c
vmlankub <63699530+urlib-bot@users.noreply.github.com>
2020-05-01 13:10:50 +0800

    [I] src: https://static.isab.top/index.js

---
+++ f2/62/84/66/f262846668a4d49770fe4d917d5838d90545b0e72152e562b9ecd28c231ad353.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>ERROR 403</title>
+
+<script type="2a496634a4813e1075e47ae0-text/javascript">
+        window.ga = window.ga || function () { (ga.q = ga.q || []).push(arguments) }; ga.l = +new Date;
+        ga('create', 'UA-116309064-4', { 'siteSpeedSampleRate': 100, 'alwaysSendReferrer': true });
+        ga('set', 'forceSSL', true);
+        ga('send', 'pageview');
+    </script>
+<script async src='https://www.google-analytics.com/analytics.js' type="2a496634a4813e1075e47ae0-text/javascript"></script>
+</head>
+<body>
+<h1 id="error-403">ERROR 403</h1>
+<p>
+<b>Forbidden! </b>
+<br />
+You are not authorized to access this page. Please contact <a href="/support/about-403-forbidden.html">our
+support team</a>
+for help.
+</p>
+<p>
+<b>New! </b>
+<br />
+Try our <a href="/speedtest.html">Internet Speed Test</a> page.
+</p>
+<p>
+Served by <a href="https://www.cloudflare.com/">Cloudflare</a>.
+More info about your connection:
+<iframe frameborder="0" width="100%" marginheight="0" marginwidth="0" scrolling="yes" src="/cdn-cgi/trace"></iframe>
+</p>
+<script src="https://ajax.cloudflare.com/cdn-cgi/scripts/7089c43e/cloudflare-static/rocket-loader.min.js" data-cf-settings="2a496634a4813e1075e47ae0-|49" defer=""></script></body>
+</html>
